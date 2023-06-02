64b6450c8a3852f659aad3a50c0d1aedcb62ff9f
Lucas Paul <l.b.paul@student.tudelft.nl>
2023-06-02 14:46:26 +0200

    better and clearer test methods and more tests

--- test/LikelihoodUtilsTest.java
+++ test/LikelihoodUtilsTest.java
@@ -1,36 +1,45 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class LikelihoodUtilsTest {
 
-    @Test
-    void testBernoulliLikelihood() {
-        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(LikelihoodUtils.BERNOULLI, -1, null, 1,1,1,1));
-        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(LikelihoodUtils.BERNOULLI, 0.5, null));
-        double result1 = 1*0.8*0.8*0.8*0.8*(1-0.8);
-        double result2 = 1*0.5*0.5*0.5*0.5*(1-0.5);
-        assertEquals(result1, LikelihoodUtils.likelihood(LikelihoodUtils.BERNOULLI, 0.8, null, 1, 1, 1, 1, 0));
-        assertEquals(result2, LikelihoodUtils.likelihood(LikelihoodUtils.BERNOULLI, 0.5, null, 1, 1, 1, 1, 0));
-        assertEquals(0, LikelihoodUtils.likelihood(LikelihoodUtils.BERNOULLI, 0.5, null, 2,1,1,1));
+    @ParameterizedTest
+    @MethodSource("invalidTests")
+    void testInvalidInputs(int distribution, double arg1, Double arg2, int... values){
+        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(distribution, arg1, arg2, values));
     }
 
-    @Test
-    void testBinomialLikelihood(){
-        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(LikelihoodUtils.BINOMIAL, 0.5, null, 1));
-        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(LikelihoodUtils.BINOMIAL, 3, -1.0, 1));
-        assertThrows(IllegalArgumentException.class, () -> LikelihoodUtils.likelihood(LikelihoodUtils.BINOMIAL, 0.5, 0.8, 1));
-        double result1 = 1 * (3 * Math.pow(0.8, 1) * Math.pow((1-0.8), 2)) * (3 * Math.pow(0.8, 2) * Math.pow((1-0.8), 1)) * (1 * Math.pow(0.8, 3) * Math.pow((1-0.8), 0)) * (1 * Math.pow(0.8, 0) * Math.pow((1-0.8), 3));
-        double result2 = 1 * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4)) * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4)) * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4));
-        assertEquals(result1, LikelihoodUtils.likelihood(LikelihoodUtils.BINOMIAL, 3, 0.8, 1, 2, 3, 0));
-        assertEquals(result2, LikelihoodUtils.likelihood(LikelihoodUtils.BINOMIAL, 5, 0.35, 1, 1, 1));
+    @ParameterizedTest
+    @MethodSource("validTests")
+    void testValidInputs(double expectation, int distribution, double arg1, Double arg2, int... values) {
+        assertEquals(expectation, LikelihoodUtils.likelihood(distribution, arg1, arg2, values));
     }
 
-    @Test
-    void testGeometricLikelihood(){
+    public static Stream<Arguments> invalidTests() {
+        return Stream.of(
+                Arguments.of(-1, -1, null, new int[]{}),
+                Arguments.of(LikelihoodUtils.BERNOULLI, -1, null, new int[]{1,1,1,1}),
+                Arguments.of(LikelihoodUtils.BERNOULLI, 0.5, null, new int[]{}),
+                Arguments.of(LikelihoodUtils.BINOMIAL, 0.5, null, new int[]{1}),
+                Arguments.of(LikelihoodUtils.BINOMIAL, 3, -1.0, new int[]{1}),
+                Arguments.of(LikelihoodUtils.BINOMIAL, 0.5, 0.8, new int[]{1})
+        );
     }
 
-    @Test
-    void testPoissonLikelihood(){
+    public static Stream<Arguments> validTests(){
+        return Stream.of(
+                Arguments.of(1*0.8*0.8*0.8*0.8*(1-0.8), LikelihoodUtils.BERNOULLI, 0.8, null, new int[]{1, 1, 1, 1, 0}),
+                Arguments.of(1*0.5*0.5*0.5*0.5*(1-0.5), LikelihoodUtils.BERNOULLI, 0.5, null, new int[]{1, 1, 1, 1, 0}),
+                Arguments.of(0, LikelihoodUtils.BERNOULLI, 0.5, null, new int[]{2,1,1,1}),
+                Arguments.of(1 * (3 * Math.pow(0.8, 1) * Math.pow((1-0.8), 2)) * (3 * Math.pow(0.8, 2) * Math.pow((1-0.8), 1)) * (1 * Math.pow(0.8, 3) * Math.pow((1-0.8), 0)) * (1 * Math.pow(0.8, 0) * Math.pow((1-0.8), 3)), LikelihoodUtils.BINOMIAL, 3, 0.8, new int[]{1, 2, 3, 0}),
+                Arguments.of(1 * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4)) * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4)) * (5 * Math.pow(0.35, 1) * Math.pow((1-0.35), 4)), LikelihoodUtils.BINOMIAL, 5, 0.35, new int[]{1, 1, 1}),
+                Arguments.of(0, LikelihoodUtils.BINOMIAL, 3, 0.6, new int[]{5,4}),
+                Arguments.of(0, LikelihoodUtils.BINOMIAL, 3, 0.6, new int[]{-1,3})
+        );
     }
 
 }
