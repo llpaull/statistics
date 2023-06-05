aa58bd8b331a99d923a20c23b2dca43ac9430d9e
Lucas Paul <l.b.paul@student.tudelft.nl>
2023-06-05 09:41:56 +0200

    added file

---
+++ src/Distribution.java
@@ -0,0 +1,15 @@
+public class Distribution {
+    enum Discrete{
+        BERNOULLI,
+        BINOMIAL,
+        GEOMETRIC,
+        POISSON
+    }
+
+    enum Continuous{
+        UNIFORM,
+        Pareto,
+        NORMAL,
+        EXPONENTIAL
+    }
+}
